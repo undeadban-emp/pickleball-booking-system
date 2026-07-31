623aa81fd2eccee9b497c0b1680d70ad5e9d78e9
unknown <boltdebug@gmail.com>
2026-07-31 10:34:05 +0800

    update the font style of  paddle ground tandag bandname in welcome, nav, app.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,10 +10,12 @@
         <meta name="description" content="{{ $description ?? 'Book pickleball courts by the hour. Pick a time, pay by GCash, show your code at the door.' }}">
 
         <link rel="icon" href="{{ \App\Models\OperatingHours::current()->logoUrl() ?? asset('favicon.ico') }}">
+        <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
 
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('head')
     </head>
     <body class="bg-white text-ink-900 antialiased dark:bg-ink-950 dark:text-ink-50">
         @include('partials.nav')
